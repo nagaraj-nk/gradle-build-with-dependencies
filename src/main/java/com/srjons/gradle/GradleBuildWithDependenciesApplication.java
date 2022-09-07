570f3c71d1250783d08b8453efef9c70f9b4b3c6
Nagaraj N <nnagarajece31@gmail.com>
2022-09-07 21:10:20 +0530

    Initial commit

---
+++ src/main/java/com/srjons/gradle/GradleBuildWithDependenciesApplication.java
@@ -0,0 +1,13 @@
+package com.srjons.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleBuildWithDependenciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleBuildWithDependenciesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srjons/gradle/GradleBuildWithDependenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srjons.gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleBuildWithDependenciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
